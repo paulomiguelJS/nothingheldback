e9de659e0850a753ec59b87137c5c3f90b370244
Paulo <paulomiguelblues@gmail.com>
2022-04-08 13:59:28 -0400

    Chat modal styled and responsive

--- app/modal.js
+++ app/modal.js
@@ -1,24 +1,13 @@
-var modal = document.getElementById("myModal");
+const modal = document.querySelector("#myModal");
+const openModal = document.querySelector("#btn-modal");
+const closeModal = document.querySelector("#close");
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn-modal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+openModal.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeModal.onclick = function(event) {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
